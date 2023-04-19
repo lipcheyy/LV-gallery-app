74ba0b74db783350298480a515d2c98fc294b1c7
Wair <1337excuseme@gmail.com>
2023-04-19 16:29:13 +0300

    feat: created add category component

--- resources/js/components/Admin/Category/CreateComponent.vue
+++ resources/js/components/Admin/Category/CreateComponent.vue
@@ -1,8 +1,30 @@
 <template>
-    <div>
-        input category title
-        <input v-model="title" type="text">
-        <a href="#" @click.prevent="create()">create</a>
+    <div class="categoryBlock">
+        <h1 class="h1Category">Головна сторінка категорії</h1>
+        <h3 class="h2Category">Назва категорії введення</h3>
+        <form class="formAddCategory" action="#" method="#">
+            <input class = "inputCategory" v-model="title" type="text" placeholder="Назва">
+            <a class = "buttonCategory" href="#" @click.prevent="create()">Створити</a>
+        </form>
+        <table class="categoryTable">
+            <thead>
+            <tr>
+                <td class="firstConTable">ID</td>
+                <td class="secConTable">Назва</td>
+                <td class="thrdConTable">Дії</td>
+            </tr>
+            </thead>
+            <tr>
+                <td>Breed</td>
+                <td>Jack Russell</td>
+                <td>
+                    <div class="logoTable">
+                        <a href="#"><img class="tableLogo"src="img/edit.png" alt="edit"></a>
+                        <a href="#"><img class="tableLogo" src="img/del.png" alt="del"></a>
+                    </div>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
