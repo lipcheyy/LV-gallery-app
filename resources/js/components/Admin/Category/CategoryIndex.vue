2e1a6571c4d786e7033585f32a8a50d3f7e29ad9
Wair <1337excuseme@gmail.com>
2023-04-19 19:44:30 +0300

    add: table and style

--- resources/js/components/Admin/Category/CategoryIndex.vue
+++ resources/js/components/Admin/Category/CategoryIndex.vue
@@ -4,31 +4,35 @@
 
         <create-component ref="test"></create-component>
 
-        <table class="table w-25">
-            <thead>
+        <div class="TableBlock">
+            <table class="categoryTable">
+                <thead>
                 <tr>
-                    <th>#</th>
-                    <th>title</th>
-                    <th colspan="2">actions</th>
+                    <td class="firstConTable">ID</td>
+                    <td class="secConTable">Назва</td>
+                    <td class="thrdConTable">Дії</td>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 <template v-for="category in data">
                     <tr :class="categoryToEdit(category.id)?'d-none':''">
                         <td>{{category.id}}</td>
                         <td>{{category.title}}</td>
-                        <td><a href="#" @click.prevent="getCategoryDataToEdit(category.id,category.title)">edit</a></td>
-                        <td><a href="#">delete</a></td>
-                    </tr>
-<!--                    editing fields-->
+                        <td>
+                            <div class="logoTable">
+                                <a href="#" @click.prevent="getCategoryDataToEdit(category.id,category.title)"><img class="tableLogo"src="img/edit.png" alt="edit"></a>
+                                <a href="#"><img class="tableLogo" src="img/del.png" alt="del"></a>
+                            </div>
+                        </td>
                     <tr :class="categoryToEdit(category.id)?'':'d-none'">
                         <td>{{category.id}}</td>
                         <td><input v-model="title" type="text" class="form-control" ></td>
                         <td><a href="#" @click.prevent="update(category.id)">update</a></td>
                     </tr>
                 </template>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -77,4 +81,67 @@ export default {
 
 <style scoped>
 
+
+.tableCategory{
+    width: 892px;
+    height: 351px;
+
+    background: #F1F1F1;
+    border: 3px solid #323232;
+    box-shadow: 5px -4px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 17px;
+}
+.theadCategory{
+    background: #4D4D4D;
+}
+.categoryTable {
+    width: 815px;
+    margin-bottom: 20px;
+    background: #F1F1F1;
+    border: 3px solid #323232;
+    box-shadow: 5px -4px 10px rgba(0, 0, 0, 0.25);
+    border-collapse: collapse;
+    border-radius: 17px;
+}
+.categoryTable td {
+    border: 3px solid #323232;
+    padding: 5px;
+}
+.firstConTable{
+    width: 10%;
+}
+.secConTable{
+    width: 57%;
+}
+
+.categoryTable thead{
+    background: #4D4D4D;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 29px;
+    text-align: center;
+    letter-spacing: 0.14em;
+    color: #FFFFFF;
+}
+.tableLogo{
+    width: 16px;
+    height: 20px;
+}
+.table tr{
+    height: 50px;
+}
+.logoTable{
+    display: flex;
+    justify-content: center;
+    gap: 46%;
+}
+.TableBlock{
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
 </style>
