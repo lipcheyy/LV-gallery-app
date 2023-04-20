47f64d13fec89092330df6f2212227844f297d4b
Vladislav <vladlipchey05@gmail.com>
2023-04-20 20:34:59 +0300

    initialize dropzone element

--- resources/js/components/Post/CreatePost.vue
+++ resources/js/components/Post/CreatePost.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-        create
+    <div class="dropzone_container">
+        <div ref="dropzone" class="bg-info p-5">
+            dropzone
+        </div>
     </div>
 </template>
 
 <script>
+import Dropzone from 'dropzone'
 export default {
-    name: "CreatePost"
+    name: "CreatePost",
+    data(){
+        return{
+            dropzone:null
+        }
+    },
+    mounted() {
+        this.dropzone=new Dropzone(this.$refs.dropzone,{
+            url:'#'
+        })
+    }
 }
 </script>
 
 <style scoped>
-
+    .dropzone_container{
+        width: 150px;
+        height: 120px;
+    }
 </style>
