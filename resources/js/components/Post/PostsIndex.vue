5117b33590b0581bbf68f3674866ba90603995e9
Vladislav <vladlipchey05@gmail.com>
2023-04-21 10:08:34 +0300

    create post layouts

---
+++ resources/js/components/Post/PostsIndex.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <post-layout></post-layout>
+    </div>
+</template>
+
+<script>
+import PostLayout from "./PostLayout";
+export default {
+    name: "PostsIndex",
+    components: {PostLayout}
+}
+</script>
+
+<style scoped>
+
+</style>
