845232f6c34f2f9485ae40ed55790aa4b5ef7b17
Vladislav <vladlipchey05@gmail.com>
2023-05-08 23:34:25 +0300

    change returned post capacity

--- app/Http/Controllers/Post/PostsController.php
+++ app/Http/Controllers/Post/PostsController.php
@@ -17,7 +17,7 @@ class   PostsController extends Controller
         $data=\request()->validate(
             ['page'=>'integer']
         );
-        $posts = Post::paginate(5,['*'],'page',$data['page']);
+        $posts = Post::paginate(12,['*'],'page',$data['page']);
         return PostResource::collection($posts);
     }
     public function checkUserLiked(){
